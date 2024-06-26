d09c49fe4a116fec19b59a79b0e75a797172dff9
mohammedabarh <abmohammed606@gmail.com>
2024-06-25 19:43:46 -0700

    rrr

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,30 +7,33 @@
  */
 void times_table(void)
 {
-    int a, b, op;
+	int row, col, product;
 
-    for (a = 0; a <= 9; a++)
-    {
-        _putchar('0');  // Print the initial '0' for the first column
-        for (b = 1; b <= 9; b++)
-        {
-            op = a * b;
-            _putchar(',');      // Print the comma separator
-            _putchar(' ');      // Print the space after the comma
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			product = row * col;
 
-            // Print the result with correct formatting
-            if (op <= 9)
-            {
-                _putchar(' ');  // Extra space for alignment
-                _putchar(op + '0');
-            }
-            else
-            {
-                _putchar((op / 10) + '0');
-                _putchar((op % 10) + '0');
-            }
-        }
-        _putchar('\n');  // Move to the next line after each row
-    }
+			if (col == 0)
+				_putchar('0');
+			else if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+
+			if (col < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
-
