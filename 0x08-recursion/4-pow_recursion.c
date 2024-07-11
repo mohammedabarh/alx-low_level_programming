026b7c455d2ccfe9bc4bf74a733d8a0fb663b00c
mohammedabarh <abmohammed606@gmail.com>
2024-07-10 21:46:53 -0700

    0x08-recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,47 +1,18 @@
 #include "main.h"
 
 /**
-
-
-* factorial - gets factorial of n
-
-
-* @n: integer
-
-
-*
-
-
-* Return: On success 1.
-
-
-* On error, -1 is returned, and errno is set appropriately.
-
-
-*/
-
-
-int factorial(int n)
-
-
+ * _pow_recursion - Calculates the value of x raised to the power of y
+ * @x: The base number
+ * @y: The exponent
+ *
+ * Return: The result of x raised to the power of y
+ *         -1 if y is lower than 0
+ */
+int _pow_recursion(int x, int y)
 {
-
-
-	if (n < 0)
-
-
-		return (-1);
-
-
-	if (n == 0)
-
-
-		return (1);
-
-
-	return (n * factorial(n - 1));
-
-
+	if (y < 0)
+		return -1;
+	if (y == 0)
+		return 1;
+	return x * _pow_recursion(x, y - 1);
 }
-
-
