ffcbc2b377b1481de5d81eb6bb053d37637d91e2
mohammedabarh <abmohammed606@gmail.com>
2024-07-15 15:46:28 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,37 +9,37 @@
  */
 char *argstostr(int ac, char **av)
 {
-    char *str;
-    int i, j, k, len;
+	char *str;
+	int i, j, k, len;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    /* Calculate the total length of the new string */
-    len = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-            len++;
-        len++; /* For the newline character */
-    }
+	/* Calculate the total length of the new string */
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++; /* For the newline character */
+	}
 
-    /* Allocate memory for the new string */
-    str = malloc(sizeof(char) * (len + 1));
-    if (str == NULL)
-        return (NULL);
+	/* Allocate memory for the new string */
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
 
-    /* Copy the arguments into the new string */
-    k = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j]; j++)
-        {
-            str[k++] = av[i][j];
-        }
-        str[k++] = '\n';
-    }
-    str[k] = '\0';
+	/* Copy the arguments into the new string */
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k++] = av[i][j];
+		}
+		str[k++] = '\n';
+	}
+	str[k] = '\0';
 
-    return (str);
+	return (str);
 }
