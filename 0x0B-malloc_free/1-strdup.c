3f763cf6a6766f2907c2c4e7a3a6ad898b1ed9c0
mohammedabarh <abmohammed606@gmail.com>
2024-07-15 13:02:53 -0700

    1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,23 +10,19 @@
  */
 char *_strdup(char *str)
 {
-	if (str == NULL) {
-		return NULL;
-	}
-	
-	int length = 0;
-	while (str[length] != '\0') {
-		length++;
-	}
-	
-	char *duplicate = malloc(sizeof(char) * (length + 1));
-		
-	if (duplicate == NULL) {
-		return NULL;
-	}
-	
-	for (int i = 0; i <= length; i++) {
-		duplicate[i] = str[i]
-	}
-	return duplicate;
+    if (str == NULL)
+        return NULL;
+
+    int length = 0;
+    while (str[length] != '\0')
+        length++;
+
+    char *duplicate = malloc(sizeof(char) * (length + 1));
+    if (duplicate == NULL)
+        return NULL;
+
+    for (int i = 0; i <= length; i++)
+        duplicate[i] = str[i];
+
+    return duplicate;
 }
