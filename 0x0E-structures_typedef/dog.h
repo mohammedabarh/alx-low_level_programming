00fd59c8b32b609b0a0630f30c868655f7382e63
mohammedabarh <abmohammed606@gmail.com>
2024-07-22 16:45:51 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,21 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
 
 
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+   /**
+    * struct dog - dog class
+    *
+    * @name: Name of the dog
+    *
+    * @age: Age of the dog
+    *
+    * @owner: Owner of the dog
+    */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
