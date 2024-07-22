50673e29027aa7a113b5b76b458e9347b35994b5
mohammedabarh <abmohammed606@gmail.com>
2024-07-22 14:54:45 -0700

    0-object_like_macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef SIZE_MACRO_H
+#define SIZE_MACRO_H
 
-static const int SIZE = 1024;
+#define SIZE 1024
 
-#endif /* OBJECT_LIKE_MACRO_H */
+#endif /* SIZE_MACRO_H */
