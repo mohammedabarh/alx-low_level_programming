1fd37c6033ec2e939a30315790a5fa90f9e48557
mohammedabarh <abmohammed606@gmail.com>
2024-07-25 17:24:14 -0700

    3-print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,89 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_char - Prints a character.
+ * @val: The character to print.
+ */
+void print_char(int val)
+{
+	printf("%c", val);
+}
+
+/**
+ * print_int - Prints an integer.
+ * @val: The integer to print.
+ */
+void print_int(int val)
+{
+	printf("%d", val);
+}
+
+/**
+ * print_float - Prints a float.
+ * @val: The float to print.
+ */
+void print_float(double val)
+{
+	printf("%f", val);
+}
+
+/**
+ * print_string - Prints a string.
+ * @val: The string to print.
+ */
+void print_string(char *val)
+{
+	if (val == NULL)
+		printf("(nil)");
+	else
+		printf("%s", val);
+}
 
 /**
  * print_all - Prints anything based on the format specifiers.
- * @format: The format string containing the types of arguments.
+ * @format: A string representing the format of the arguments.
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
+	va_list valist;
 	unsigned int i = 0;
-	char *str;
-	char *separator = "";
+	char *sep = "";
 
-	va_start(args, format);
+	va_start(valist, format);
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		if (i > 0)
+			printf("%s", sep);
+
+		switch (format[i])
 		{
-			printf("%s%c", separator, va_arg(args, int));
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%s%d", separator, va_arg(args, int));
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%s%f", separator, va_arg(args, double));
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str)
-				printf("%s%s", separator, str);
-			else
-				printf("%s(nil)", separator);
+			case 'c':
+				print_char(va_arg(valist, int));
+				sep = ", ";
+				break;
+			case 'i':
+				print_int(va_arg(valist, int));
+				sep = ", ";
+				break;
+			case 'f':
+				print_float(va_arg(valist, double));
+				sep = ", ";
+				break;
+			case 's':
+				print_string(va_arg(valist, char *));
+				sep = ", ";
+				break;
+			default:
+				i++;
+				continue;
 		}
 		i++;
-		if (format[i])
-			separator = ", ";
 	}
 
-	va_end(args);
-
 	printf("\n");
+	va_end(valist);
 }
 
