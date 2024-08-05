1a1018a90e5aa667e888a179eee890d4abc32263
mohammedabarh <abmohammed606@gmail.com>
2024-08-05 16:17:40 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,24 +1,27 @@
+/*
+ * File: 0-print_listint.c
+ * Auth: Gedeon Obae Gekonge
+ */
+
 #include "lists.h"
-#include <unistd.h> /* For _putchar */
+#include <stdio.h>
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
- * @h: Pointer to the head of the list.
+ * @h: A pointer to the head of the list_t list.
  *
- * Return: The number of nodes in the list.
+ * Return: The number of nodes in the list_t list.
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		_putchar(h->n + '0'); /* Convert integer to character */
-		_putchar('\n');      /* Print newline */
-		count++;             /* Increment the node counter */
-		h = h->next;         /* Move to the next node */
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 
-	return (count);
+	return (nodes);
 }
-
