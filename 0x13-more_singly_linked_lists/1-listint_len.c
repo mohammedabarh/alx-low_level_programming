6bf0862e19a5572207dede7d31c728709a7a1736
mohammedabarh <abmohammed606@gmail.com>
2024-08-05 18:12:01 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,18 +6,18 @@
  *               in a listint_t linked list.
  * @h: Pointer to the first node of the listint_t list.
  *
- * Return: The count of nodes in the listint_t list.
+ * Return: The number of nodes in the listint_t list.
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	size_t number = 0;
 
 	while (h != NULL)
 	{
-		count++;
+		number++;
 		h = h->next;
 	}
 
-	return (count);  /* Added parentheses here */
+	return (number);  /* Added parentheses here */
 }
 
