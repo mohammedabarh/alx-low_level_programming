dfcaed28eac7501ba99b6769dc1b9ca6bf309709
mohammedabarh <abmohammed606@gmail.com>
2024-09-19 21:08:45 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,50 +1,53 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: double pointer to the head of the list.
- * @idx: index where the new node should be added (starts at 0).
- * @n: value to store in the new node.
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
+ * @h: Double pointer to the head of the list.
+ * @idx: Index where the new node should be added (starts at 0).
+ * @n: Data to insert into the new node.
  *
- * Return: address of the new node, or NULL if it failed.
+ * Return: Address of the new node, or NULL if it failed.
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *aux_node = *h, *new_node;
-	unsigned int index, cont = 0;
+	dlistint_t *new_node, *temp = *h;
+	unsigned int i = 0;
 
+	/* Create the new node */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 
+	/* Case: Inserting at the beginning (index 0) */
 	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	/* Traverse the list to find the position to insert the node */
+	while (temp != NULL && i < idx - 1)
 	{
-		new_node->prev = NULL;
-		new_node->next = *h;
-		if (*h)
-			(*h)->prev = new_node;
-		*h = new_node;
-		return (*h);
+		temp = temp->next;
+		i++;
 	}
 
-	index = idx - 1;
-	while (aux_node && cont != index)
+	/* If temp is NULL, the index is out of bounds */
+	if (temp == NULL || temp->next == NULL)
 	{
-		cont++;
-		aux_node = aux_node->next;
+		free(new_node);
+		return (NULL);
 	}
 
-	if (cont == index && aux_node)
-	{
-		new_node->prev = aux_node;
-		new_node->next = aux_node->next;
-		if (aux_node->next)
-			aux_node->next->prev = new_node;
-		aux_node->next = new_node;
-		return (new_node);
-	}
-	free(new_node);
-	return (NULL);
+	/* Case: Inserting at the end */
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	/* Insert new node between temp and temp->next */
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+
+	return (new_node);
 }
+
