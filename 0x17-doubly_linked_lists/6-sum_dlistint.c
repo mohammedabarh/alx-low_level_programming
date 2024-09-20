991c198cbb7242373d365ed052c1cb4d3a8537ad
mohammedabarh <abmohammed606@gmail.com>
2024-09-19 21:13:31 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,46 +1,20 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list.
- * @head: double pointer to the head of the list.
- * @index: index of the node to delete (starting from 0).
- *
- * Return: 1 if it succeeded, -1 if it failed.
- */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t list.
+ * @head: pointer to the list.
+ * Return: sum of all data in the list, 0 if the lost is empty.
+ **/
+int sum_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp = *head;
-	unsigned int i = 0;
+	dlistint_t *node = head;
+	int sum = 0;
 
-	if (*head == NULL)  /* List is empty */
-		return (-1);
-
-	if (index == 0)  /* Delete the head node */
+	while (node)
 	{
-		*head = temp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(temp);
-		return (1);
+		sum += node->n;
+		node = node->next;
 	}
 
-	/* Traverse to the node at the given index */
-	while (temp != NULL && i < index)
-	{
-		temp = temp->next;
-		i++;
-	}
-
-	if (temp == NULL)  /* Index is out of range */
-		return (-1);
-
-	if (temp->next != NULL)  /* Adjust the next node's previous pointer */
-		temp->next->prev = temp->prev;
-
-	if (temp->prev != NULL)  /* Adjust the previous node's next pointer */
-		temp->prev->next = temp->next;
-
-	free(temp);  /* Free the node */
-	return (1);
+	return (sum);
 }
-
