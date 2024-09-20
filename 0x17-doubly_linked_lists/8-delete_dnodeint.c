7c728136e1f82f92b42b2ee62b03d2271b772820
mohammedabarh <abmohammed606@gmail.com>
2024-09-19 21:02:32 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,38 +9,38 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *temp = *head;
-    unsigned int i = 0;
+	dlistint_t *temp = *head;
+	unsigned int i = 0;
 
-    if (*head == NULL)  /* List is empty */
-        return (-1);
+	if (*head == NULL)  /* List is empty */
+		return (-1);
 
-    if (index == 0)  /* Delete the head node */
-    {
-        *head = temp->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(temp);
-        return (1);
-    }
+	if (index == 0)  /* Delete the head node */
+	{
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
 
-    /* Traverse to the node at the given index */
-    while (temp != NULL && i < index)
-    {
-        temp = temp->next;
-        i++;
-    }
+	/* Traverse to the node at the given index */
+	while (temp != NULL && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
 
-    if (temp == NULL)  /* Index is out of range */
-        return (-1);
+	if (temp == NULL)  /* Index is out of range */
+		return (-1);
 
-    if (temp->next != NULL)  /* Adjust the next node's previous pointer */
-        temp->next->prev = temp->prev;
+	if (temp->next != NULL)  /* Adjust the next node's previous pointer */
+		temp->next->prev = temp->prev;
 
-    if (temp->prev != NULL)  /* Adjust the previous node's next pointer */
-        temp->prev->next = temp->next;
+	if (temp->prev != NULL)  /* Adjust the previous node's next pointer */
+		temp->prev->next = temp->next;
 
-    free(temp);  /* Free the node */
-    return (1);
+	free(temp);  /* Free the node */
+	return (1);
 }
 
