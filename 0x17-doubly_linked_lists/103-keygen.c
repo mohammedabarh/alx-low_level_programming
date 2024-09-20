f44d1d9054fdbfd60e96c9bd6626ba9117572a11
mohammedabarh <abmohammed606@gmail.com>
2024-09-19 21:33:59 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -2,73 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * generate_key - Generates a key based on the username
- * @username: The username to generate the key for
- * @key: The buffer to store the generated key
- */
-void generate_key(const char *username, char *key)
-{
-    size_t i, len;
-    int ch;
-    long sum;
+#define MAX_USERNAME_LENGTH 100
 
-    // Initialize key generation
-    key[0] = 'O';
-    key[1] = 'G';
-    key[2] = username[0];
-    key[3] = 'H';
-    key[4] = '0';
-    key[5] = '4';
+unsigned long hash(const char *str) {
+    unsigned long hash = 5381;
+    int c;
 
-    // Calculate sum of ASCII values in username
-    len = strlen(username);
-    sum = 0;
-    for (i = 0; i < len; i++)
-        sum += username[i];
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-    // Set the 6th character based on sum
-    key[6] = ((sum ^ 79) & 63) + 64;
-
-    // Generate the rest of the key
-    ch = 1;
-    for (i = 0; i < len; i++)
-        ch *= username[i];
-    key[7] = ((ch ^ 85) & 63) + 64;
-
-    ch = username[0];
-    for (i = 0; i < len; i++)
-        if (username[i] > ch)
-            ch = username[i];
-    key[8] = ((ch ^ 14) & 63) + 64;
-
-    for (i = 0; i < len; i++)
-        if (username[i] <= ch && username[i] > key[8])
-            key[8] = username[i];
-    key[8] = ((key[8] ^ 34) & 63) + 64;
-
-    // Finalize key
-    key[9] = '\0';
+    return hash;
 }
 
-/**
- * main - Entry point, generates a key for crackme5
- * @argc: Argument count
- * @argv: Argument vector
- * Return: 0 on success, 1 on error
- */
-int main(int argc, char *argv[])
-{
-    char key[10];
+char generate_key_char(int index, const char *username) {
+    unsigned long username_hash = hash(username);
+    char key_char = ((username_hash ^ (index * 1337)) % 26) + 'A';
+    return key_char;
+}
 
-    if (argc != 2)
-    {
+void generate_key(const char *username, char *key) {
+    int key_length = strlen(username) + 4;
+    for (int i = 0; i < key_length; i++) {
+        key[i] = generate_key_char(i, username);
+    }
+    key[key_length] = '\0';
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         fprintf(stderr, "Usage: %s username\n", argv[0]);
-        return (1);
+        return 1;
     }
 
-    generate_key(argv[1], key);
+    char username[MAX_USERNAME_LENGTH];
+    strncpy(username, argv[1], MAX_USERNAME_LENGTH - 1);
+    username[MAX_USERNAME_LENGTH - 1] = '\0';
+
+    char key[MAX_USERNAME_LENGTH + 5];
+    generate_key(username, key);
+
     printf("%s\n", key);
 
-    return (0);
+    return 0;
 }
