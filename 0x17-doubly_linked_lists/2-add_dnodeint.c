2e5cd6985329290c0713e8fe10cefbf0e9261f0f
mohammedabarh <abmohammed606@gmail.com>
2024-09-19 18:40:00 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,32 +1,27 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
- * @head: double pointer to the first element of the list.
- * @n: integer value to be stored in the new node.
- * Return: address of the new element, or NULL if it failed.
- **/
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @n: Integer value to be stored in the new node.
+ *
+ * Return: Address of the new element, or NULL if it failed.
+ */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *new_node;
 
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Initialize the new node */
-    new_node->n = n;
-    new_node->prev = NULL;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head)
+		(*head)->prev = new_node;
+	*head = new_node;
 
-    /* Link the new node to the current head */
-    new_node->next = *head;
-
-    /* Update the previous head's prev pointer if it exists */
-    if (*head != NULL)
-        (*head)->prev = new_node;
-
-    /* Update head to point to the new node */
-    *head = new_node;
-
-    return (new_node);
+	return (new_node);
 }
-
