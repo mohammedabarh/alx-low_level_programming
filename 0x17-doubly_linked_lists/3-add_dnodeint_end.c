de097248a22eca655301edf153381a4f78a4bd39
mohammedabarh <abmohammed606@gmail.com>
2024-09-19 18:51:07 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,45 +1,44 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
- * @head: double pointer to the first element of the list.
- * @n: integer value to set in the new node.
- * 
- * Return: address of the new element, or NULL if it failed.
- **/
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
+ * @head: Pointer to the pointer of the first element of the list.
+ * @n: Integer to be set in the new node.
+ * Return: Address of the new element, or NULL if it failed.
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
     dlistint_t *new_node;
-    dlistint_t *last_node = *head;
+    dlistint_t *current;
 
     /* Allocate memory for the new node */
     new_node = malloc(sizeof(dlistint_t));
     if (new_node == NULL)
-    {
-        return (NULL);  /* Return NULL if memory allocation fails */
-    }
+        return (NULL);
 
-    /* Set the values for the new node */
+    /* Initialize the new node */
     new_node->n = n;
-    new_node->next = NULL;  /* The new node will be the last node */
+    new_node->next = NULL;
 
-    /* If the list is empty, set the new node as the head */
+    /* If the list is empty, make the new node the head */
     if (*head == NULL)
     {
         new_node->prev = NULL;
         *head = new_node;
-    }
-    else
-    {
-        /* Traverse to the end of the list */
-        while (last_node->next)
-        {
-            last_node = last_node->next;
-        }
-        /* Update pointers to add the new node at the end */
-        last_node->next = new_node;
-        new_node->prev = last_node;
+        return (new_node);
     }
 
-    return (new_node);  /* Return the address of the new node */
+    /* Traverse to the last node */
+    current = *head;
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+
+    /* Link the new node at the end */
+    current->next = new_node;
+    new_node->prev = current;
+
+    return (new_node);
 }
