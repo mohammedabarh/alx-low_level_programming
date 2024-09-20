07cff1f69738c8fe81bb921f96d8bd5c5269c6f6
mohammedabarh <abmohammed606@gmail.com>
2024-09-19 17:55:26 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,13 +8,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t node_count = 0;
-
-    while (h != NULL)
-    {
-        node_count++;
-        h = h->next;
-    }
-
-    return (node_count);
+	size_t cont = 0;
+	
+	while (h)
+	{
+		h = h->next;
+		cont++;
+	}
+	
+	return (cont);
 }
