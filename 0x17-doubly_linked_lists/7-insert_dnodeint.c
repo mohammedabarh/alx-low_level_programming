f152766059bbec53d9b2c1c3129c45c029b0b1cf
mohammedabarh <abmohammed606@gmail.com>
2024-09-19 20:29:12 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,38 +8,47 @@
  *
  * Return: address of the new node, or NULL if it failed.
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new, *temp = *h;
-    unsigned int i = 0;
+	dlistint_t *aux_node = *h, *new_node;
+	unsigned int index, cont = 0;
 
-    if (idx == 0) /* Insert at the beginning */
-        return (add_dnodeint(h, n));
+	/* create node */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
 
-    /* Traverse the list to find the position before the index */
-    while (temp && i < idx - 1)
-    {
-        temp = temp->next;
-        i++;
-    }
+	/* border case for insert at the beginning */
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (*h);
+	}
 
-    if (temp == NULL) /* Index is out of range */
-        return (NULL);
+	/* search of position to insert */
+	index = idx - 1;
+	while (aux_node && cont != index)
+	{
+		cont++;
+		aux_node = aux_node->next;
+	}
 
-    if (temp->next == NULL) /* Insert at the end */
-        return (add_dnodeint_end(h, n));
-
-    /* Insert in the middle */
-    new = malloc(sizeof(dlistint_t));
-    if (!new)
-        return (NULL);
-    new->n = n;
-    new->next = temp->next;
-    new->prev = temp;
-    if (temp->next != NULL)
-        temp->next->prev = new;
-    temp->next = new;
-
-    return (new);
+	/* general case */
+	if (cont == index && aux_node)
+	{
+		new_node->prev = aux_node;
+		new_node->next = aux_node->next;
+		if (aux_node->next)
+			aux_node->next->prev = new_node;
+		aux_node->next = new_node;
+		return (new_node);
+	}
+	free(new_node);
+	return (NULL);
 }
-
