4a23332eada6854d2a58ddf73549d5ad1a395436
mohammedabarh <abmohammed606@gmail.com>
2024-11-27 17:03:28 -0800

    0x1C-makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,6 +6,7 @@
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid."""
     perimeter = 0
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
@@ -14,4 +15,5 @@ def island_perimeter(grid):
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
+
     return perimeter
