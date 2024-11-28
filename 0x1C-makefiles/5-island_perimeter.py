e7dfecc9871444b62b0d7245bcd66da11cb1fabc
mohammedabarh <abmohammed606@gmail.com>
2024-11-27 17:07:47 -0800

    0x1C-makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,39 @@
 #!/usr/bin/python3
 """
-5-island_perimeter
+Module for calculating the perimeter of an island in a grid.
 """
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid."""
+    """
+    Calculate the perimeter of the island described in grid.
+    
+    Args:
+        grid (List[List[int]]): A list of list of integers where:
+            - 0 represents water
+            - 1 represents land
+    
+    Returns:
+        int: The perimeter of the island
+    """
     perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0]) if rows else 0
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+                # Check all 4 sides
+                # Top
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                # Bottom
+                if i == rows-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                # Left
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                # Right
+                if j == cols-1 or grid[i][j+1] == 0:
+                    perimeter += 1
 
     return perimeter
